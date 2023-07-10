a8714dea316c6d521648ecd8ca1888cf63746186
ndhcuongpxu <ndhcuong.hue2023@gmail.com>
2023-07-10 13:45:52 +0700

    thay doi lan 2

--- src/pxu/edu/vn/BaiTapLon.java
+++ src/pxu/edu/vn/BaiTapLon.java
@@ -6,5 +6,54 @@ public class BaiTapLon {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	//Ham nhap so luong sinh vien N nguyen duong
+	public static int nhapSoLuongSinhVien() {
+		int n;
+		
+		return n;
+	}
+	//Ham nhap ho ten, diem Toan, Van Anh
+	public static void nhapHoTenVaDiem(String[] hoten,
+										double[] dToan,
+										double[] dVan,
+										double[] dAnh) {
+		
+	}
+	//Ham tinh diem trung binh
+	public static double[] tinhDTB(double[] dToan, double[] dVan, double[] dAnh) {
+		
+	}
+	//Ham sap xep sinh  vien theo chieu giam dan cua diem trung binh
+	public static void sapxepDSSV(String[] hoten,
+										double[] dToan,
+										double[] dVan,
+										double[] dAnh,
+										double[] dTB) {
+		
+	}
+	//Ham in danh sach sinh vien ra man hinh
+	public static void inDSSV(String[] hoten,
+										double[] dToan,
+										double[] dVan,
+										double[] dAnh,
+										double[] dTB) {
+		
+	}
+	//Ham tim kiem sinh vien theo diem mon Toan
+	public static void timkiemDiem(String[] hoten,
+										double[] dToan,
+										double[] dVan,
+										double[] dAnh,
+										double[] dTB) {
+		
+	}
+	//Ham tim kiem sinh vien theo ho ten
+	public static void timHoTen(String[] hoten,
+										double[] dToan,
+										double[] dVan,
+										double[] dAnh,
+										double[] dTB) {
+		
+	}
 }
